30e461e6765b2c73a98d1ee6a58df72d621512e7
Philipp Fromme <philippfromme@outlook.com>
2017-09-04 22:14:02 +0200

    feat(audio): change sounds, remove old sounds and svgs

--- src/config.js
+++ src/config.js
@@ -1,10 +1,4 @@
-import clapSvg from '../assets/icons/clap.svg';
-import closedhatSvg from '../assets/icons/closedhat.svg';
-import kickSvg from '../assets/icons/kick.svg';
-import openhatSvg from '../assets/icons/openhat.svg';
 import removeSvg from '../assets/icons/remove.svg';
-import snareSvg from '../assets/icons/snare.svg';
-import tomSvg from '../assets/icons/tom.svg';
 
 export default {
   maxDistance: 400, // px
@@ -24,20 +18,14 @@ export default {
   ],
   initialSound: undefined,
   sounds: [
-    { id: 'kick', label: 'Kick', path: './audio/kick-2.wav' },
-    { id: 'clap', label: 'Clap', path: './audio/clap-2.wav' },
-    { id: 'snare', label: 'Snare', path: './audio/snare.wav' },
-    { id: 'closedhat', label: 'Closed Hihat', path: './audio/closedhat-2.wav' },
-    { id: 'openhat', label: 'Open Hihat', path: './audio/openhat-1.wav' },
-    { id: 'tom', label: 'Tom', path: './audio/tom-1.wav' }
+    { id: 'kick', label: 'Kick', path: './audio/alphabetical/kick.wav' },
+    { id: 'clap', label: 'Clap', path: './audio/alphabetical/clap.wav' },
+    { id: 'snare', label: 'Snare', path: './audio/alphabetical/snare.wav' },
+    { id: 'closedhat', label: 'Closed Hihat', path: './audio/alphabetical/closedhat.wav' },
+    { id: 'openhat', label: 'Open Hihat', path: './audio/alphabetical/openhat.wav' },
+    { id: 'tom', label: 'Tom', path: './audio/alphabetical/tom.wav' }
   ],
   icons: {
-    clap: clapSvg,
-    closedhat: closedhatSvg,
-    kick: kickSvg,
-    openhat: openhatSvg,
-    remove: removeSvg,
-    snare: snareSvg,
-    tom: tomSvg
+    remove: removeSvg
   }
 };
