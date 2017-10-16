412e61c785c755c0e261bcba19e0b821c9456ab1
Nico Rehwaldt <git_nikku@nixis.de>
2017-10-16 11:03:53 +0200

    chore(bundle): expose Gitter as UMD

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'gitter.js',
+    library: 'Gitter',
+    libraryTarget: 'umd'
   },
   plugins: [
     new CopyWebpackPlugin([
